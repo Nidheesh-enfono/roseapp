a5d2eb89f28c1dcdcae1d62c7e0522fc291cbf77
Nidheesh-enfono <nidheesh@enfono.in>
2022-12-12 01:19:07 +0530

    changes

--- pages/index.tsx
+++ pages/index.tsx
@@ -5,24 +5,24 @@ import styles from "../styles/Home.module.css";
 import { Card, Row, Col, Form, Button } from "react-bootstrap";
 
 export default function Home() {
-  const [standard, setStandard] = useState(1);
+  const [standard, setStandard] = useState("1");
   const [day, setDay] = useState("TODAY");
   const [time, setTime] = useState("3:00 PM");
   const [mobile, setMobile] = useState("");
 
   const grade = [
-    { id: 1, grade: "1" },
-    { id: 2, grade: "2" },
-    { id: 3, grade: "3" },
-    { id: 4, grade: "4" },
-    { id: 5, grade: "5" },
-    { id: 6, grade: "6" },
-    { id: 7, grade: "7" },
-    { id: 8, grade: "8" },
-    { id: 9, grade: "9" },
-    { id: 10, grade: "10" },
-    { id: 11, grade: "11" },
-    { id: 12, grade: "12" },
+    { id: "1", grade: "1" },
+    { id: "2", grade: "2" },
+    { id: "3", grade: "3" },
+    { id: "4", grade: "4" },
+    { id: "5", grade: "5" },
+    { id: "6", grade: "6" },
+    { id: "7", grade: "7" },
+    { id: "8", grade: "8" },
+    { id: "9", grade: "9" },
+    { id: "10", grade: "10" },
+    { id: "11", grade: "11" },
+    { id: "12", grade: "12" },
   ];
 
   const days = [
@@ -47,35 +47,27 @@ export default function Home() {
     },
     {
       id: 2,
-      label: "3:30 PM",
-    },
-    {
-      id: 3,
       label: "4:00 PM",
     },
     {
-      id: 4,
-      label: "4:30 PM",
-    },
-    {
-      id: 5,
+      id: 3,
       label: "5:00 PM",
     },
     {
-      id: 6,
-      label: "5:30 PM",
-    },
-    {
-      id: 7,
+      id: 4,
       label: "6:00 PM",
     },
     {
-      id: 8,
-      label: "6:30 PM",
+      id: 5,
+      label: "7:00 PM",
     },
     {
-      id: 9,
-      label: "7:00 PM",
+      id: 6,
+      label: "8:00 PM",
+    },
+    {
+      id: 7,
+      label: "9:00 PM",
     },
   ];
 
@@ -93,8 +85,11 @@ export default function Home() {
   return (
     <div className={styles.container}>
       <Head>
-        <title>Roseapp</title>
-        <meta name="description" content="Roseapp" />
+        <title>Book for Free Demo Class and Counseling</title>
+        <meta
+          name="description"
+          content="Book for Free Demo Class and Counseling"
+        />
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <main>
@@ -110,13 +105,39 @@ export default function Home() {
               DEMO BOOKING
             </h3>
             <hr />
-            <h5>Select Your Child Grade in School</h5>
-            <Row className="mt-3 p-3 pb-0 pointer">
+            <h5>Select Your Child&apos;s Grade in School</h5>
+            <Row className="mt-4 pb-0 d-flex justify-content-center align-items-center">
+              <Col
+                xs={3}
+                className="p-2 m-1 rounded-2"
+                style={{
+                  border: "1px solid #F56EB3",
+                  cursor: "pointer",
+                  backgroundColor: standard === "LKG" ? "#F56EB3" : "white",
+                }}
+                onClick={() => setStandard("LKG")}
+              >
+                LKG
+              </Col>
+              <Col
+                xs={3}
+                className="p-2 m-1 rounded-2"
+                style={{
+                  border: "1px solid #F56EB3",
+                  cursor: "pointer",
+                  backgroundColor: standard === "UKG" ? "#F56EB3" : "white",
+                }}
+                onClick={() => setStandard("UKG")}
+              >
+                UKG
+              </Col>
+            </Row>
+            <Row className="p-2 pb-0 d-flex justify-content-center align-items-center">
               {grade?.map((item, idx) => (
                 <Col
                   key={idx}
                   xs={2}
-                  className="p-3 m-1 rounded-2"
+                  className="p-2 m-1 rounded-2"
                   style={{
                     border: "1px solid #F56EB3",
                     cursor: "pointer",
@@ -128,9 +149,23 @@ export default function Home() {
                 </Col>
               ))}
             </Row>
+            <Row className="mt-2 pb-0 d-flex justify-content-center align-items-center">
+              <Col
+                xs={4}
+                className="p-2 m-1 rounded-2"
+                style={{
+                  border: "1px solid #F56EB3",
+                  cursor: "pointer",
+                  backgroundColor: standard === "Adult" ? "#F56EB3" : "white",
+                }}
+                onClick={() => setStandard("Adult")}
+              >
+                Grown Up
+              </Col>
+            </Row>
             <h5 className="mt-4">Select Date for Free Demo</h5>
             <hr className="my-3" />
-            <Row className="mt-4">
+            <Row className="mt-4 px-3">
               {days?.map((item, idx) => (
                 <Col
                   key={idx}
@@ -145,6 +180,19 @@ export default function Home() {
                 </Col>
               ))}
             </Row>
+            <Row className="mt-4 d-flex justify-content-center">
+              <Col xs={6}>
+                <Form.Control
+                  style={{ border: "1px solid ##F56EB3" }}
+                  type="date"
+                  name="date"
+                  placeholder="Enter Phone Number with country code"
+                  value={day}
+                  onChange={(e) => setDay(e.target.value)}
+                />
+              </Col>
+            </Row>
+
             <Row className="py-3 mt-3 d-flex justify-content-center align-items-center">
               <h5>Select Time Slot</h5>
               <hr className="my-3" />
@@ -167,7 +215,7 @@ export default function Home() {
               ))}
             </Row>
 
-            <h5 className="mt-3">Please Enter your mobile</h5>
+            <h5 className="mt-3">Please Enter your Mobile</h5>
             <hr className="my-3" />
             <Row className="mt-3">
               <Col className="">
@@ -175,13 +223,17 @@ export default function Home() {
                   style={{ border: "1px solid ##F56EB3" }}
                   type="text"
                   name="Mobile"
-                  placeholder="Mobile"
+                  placeholder="Enter Phone Number with country code"
                   value={mobile}
                   onChange={(e) => setMobile(e.target.value)}
                 />
               </Col>
             </Row>
-            <Button onClick={handleBooking} style={{background:'#F56EB3',width:'75%'}} className="my-4">
+            <Button
+              onClick={handleBooking}
+              style={{ background: "#F56EB3", width: "100%" }}
+              className="my-4"
+            >
               Book Now
             </Button>
           </Card.Body>
